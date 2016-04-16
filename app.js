ab70397bbdadb1533d5727722819261d0ddf1719
Calvin Hamilton <chamilton025@gmail.com>
2016-04-16 10:09:01 -0400

    Initial server game implementation commit

--- app.js
+++ app.js
@@ -3,17 +3,32 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var server = app.listen(8080, function () {
-  var host = "localhost";
-  var port = server.address().port;
+var allClients = {};
+allClients['d'] = [];
+var clientNum = 1;
+io.on('connection', function(socket){
+    console.log('User %s connected!', clientNum);
+    allClients['d'].push(clientNum);
+    clientNum++;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    socket.on('disconnect', function(){
+        console.log('User disconnected');
+    });
+});
+
+server.listen(8080, function () {
+    var host = "localhost";
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
 });
 
 //Use anything from this root folder
