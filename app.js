8366c8ac91e317bb3f1ef4bb410aae27184a8f91
curryusmc <curryusmc@gmail.com>
2016-04-17 20:10:38 -0400

    Added the clients[] array for tracking connected clients.  IDs can be referenced to PlayerIDs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
+var clients = [];
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -19,6 +19,18 @@ server.listen(8080, function() {
     var port = this.address().port;
 
     console.log('Listening at http://%s:%s', host, port);
+});
+
+//Push each client to an array of clients
+
+io.on('connect', function(client) {
+    clients.push(client);
+    clients.forEach(function(client, index) {
+        console.log('Client ID: ' + index + ' is connected.');
+    });
+    client.on('disconnect', function() {
+        clients.splice(clients.indexOf(client), 1);
+    });
 });
 
 //Client connection notification 
